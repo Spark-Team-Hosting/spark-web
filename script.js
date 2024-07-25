a11ba3956efd5f2da42101769cb4a918bb6750ff
Spark-Team-Hosting <sparkbot.contact@gmail.com>
2024-07-25 15:07:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.parentElement.nextElementSibling;
+
+        button.classList.toggle('active');
+
+        if (button.classList.contains('active')) {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        } else {
+            answer.style.maxHeight = 0;
+        }
+    });
+});
